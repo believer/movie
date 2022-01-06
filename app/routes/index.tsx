d18dc477ae1208ed28292d84113065f07370f228
Rickard Natt och Dag <rickard@nattochdag.dev>
2022-01-06 19:15:13 +0100

    feat: add new movie link on feed page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -63,30 +63,34 @@ export default function Index() {
 
   return (
     <div className="grid grid-feed my-10">
-      <form
-        className="col-start-3 col-end-3 mb-5 ml-auto"
-        method="post"
-        ref={formRef}
-      >
-        <select
-          id=""
-          name="year"
-          defaultValue={data.year}
-          onChange={() => formRef.current?.submit()}
+      <div className="col-start-3 col-end-3 mb-5 flex justify-between">
+        <Link
+          className="bg-blue-600 text-white px-2 py-1 rounded text-sm hover:bg-blue-500"
+          to="/movie/new"
         >
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
-          <option value="2018">2018</option>
-          <option value="2017">2017</option>
-          <option value="2016">2016</option>
-          <option value="2015">2015</option>
-          <option value="2014">2014</option>
-          <option value="2013">2013</option>
-          <option value="2012">2012</option>
-        </select>
-      </form>
+          Add new movie
+        </Link>
+        <form method="post" ref={formRef}>
+          <select
+            id=""
+            name="year"
+            defaultValue={data.year}
+            onChange={() => formRef.current?.submit()}
+          >
+            <option value="2022">2022</option>
+            <option value="2021">2021</option>
+            <option value="2020">2020</option>
+            <option value="2019">2019</option>
+            <option value="2018">2018</option>
+            <option value="2017">2017</option>
+            <option value="2016">2016</option>
+            <option value="2015">2015</option>
+            <option value="2014">2014</option>
+            <option value="2013">2013</option>
+            <option value="2012">2012</option>
+          </select>
+        </form>
+      </div>
       {data.moviesInYear > 0 ? (
         <>
           <ul className="col-start-3 col-end-3 grid-cols-1 md:grid-cols-2 grid lg:grid-cols-4 gap-5">
