a97a264f40391159aeb9f97330329667c1a7ad9e
Rickard Natt och Dag <rickard@nattochdag.dev>
2022-01-15 08:52:54 +0100

    feat: add brand color meta tag

--- app/root.tsx
+++ app/root.tsx
@@ -32,6 +32,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="theme-color" content="#219EBC" />
         <Meta />
         <Links />
       </head>
