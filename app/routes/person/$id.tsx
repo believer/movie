105bc0c22a189316b993ce22f14935ca182de959
Rickard Natt och Dag <rickard@nattochdag.dev>
2022-02-16 08:48:51 +0100

    feat: split by job on person page

--- app/routes/person/$id.tsx
+++ app/routes/person/$id.tsx
@@ -4,10 +4,13 @@ import { H1, H2 } from '~/components/typography'
 import { yearFromDate } from '~/utils/date'
 import { db } from '~/utils/db.server'
 
+type Crew = Record<job, Array<{ movie: movie; job: job }>>
+
 type LoaderData = {
   name: string
   cast: Array<{ movie: movie; job: job }>
-  crew: Array<{ movie: movie; job: job }>
+  crew: Crew
+  hasCrew: boolean
 }
 
 export let loader: LoaderFunction = async ({ params }) => {
@@ -30,13 +33,25 @@ export let loader: LoaderFunction = async ({ params }) => {
   }
 
   const cast = person.movie_person.filter(({ job }) => job === 'cast')
-  const crew = person.movie_person.filter(({ job }) => job !== 'cast')
+  const filteredCrew = person.movie_person.filter(({ job }) => job !== 'cast')
 
-  return { name: person.name, cast, crew }
+  let crew: Crew = {
+    director: [],
+    writer: [],
+    producer: [],
+    composer: [],
+    cast: [],
+  }
+
+  for (const c of filteredCrew) {
+    crew[c.job].push(c)
+  }
+
+  return { name: person.name, cast, crew, hasCrew: filteredCrew.length > 0 }
 }
 
 export default function MoviePage() {
-  const { name, cast, crew } = useLoaderData<LoaderData>()
+  const { name, cast, crew, hasCrew } = useLoaderData<LoaderData>()
 
   return (
     <div className="my-8 mx-5">
@@ -63,28 +78,41 @@ export default function MoviePage() {
             </ul>
           </>
         )}
-        {crew.length > 0 && (
-          <>
-            <H2>Crew</H2>
-            <ul className="mt-4 grid grid-cols-1 lg:grid-cols-2 gap-1">
-              {crew.map(({ job, movie }) => (
-                <li key={movie.id} className="space-x-2">
-                  <Link
-                    className="text-brandBlue-600 underline text-sm"
-                    to={`/movie/${movie.id}`}
-                    prefetch="intent"
-                  >
-                    {movie.title}
-                  </Link>
-                  <span className="text-xs text-gray-500">
-                    ({movie.release_date && yearFromDate(movie.release_date)})
-                  </span>
-                  <span className="text-xs text-gray-500">{job}</span>
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
+        {hasCrew
+          ? Object.keys(crew).map((job) => {
+              if (crew[job as job].length === 0) {
+                return null
+              }
+
+              return (
+                <>
+                  <H2>
+                    {job.substring(0, 1).toUpperCase() + job.substring(1)}
+                  </H2>
+                  <ul className="mt-4 grid grid-cols-1 lg:grid-cols-2 gap-1">
+                    {crew[job as job].map(({ job, movie }) => (
+                      <li key={movie.id} className="space-x-2">
+                        <Link
+                          className="text-brandBlue-600 underline text-sm"
+                          to={`/movie/${movie.id}`}
+                          prefetch="intent"
+                        >
+                          {movie.title}
+                        </Link>
+                        <span className="text-xs text-gray-500">
+                          (
+                          {movie.release_date &&
+                            yearFromDate(movie.release_date)}
+                          )
+                        </span>
+                        <span className="text-xs text-gray-500">{job}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              )
+            })
+          : null}
       </div>
     </div>
   )
