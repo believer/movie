3b4598f621c2f7468c5451a53dc88fa27c1c96fe
Rickard Natt och Dag <rickard@nattochdag.dev>
2022-04-08 10:56:58 +0200

    chore: update setup to latest remix recommendations

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vitest/config'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './setupTests.ts',
+    setupFiles: ['./test/setup-test-env.ts'],
   },
 })
